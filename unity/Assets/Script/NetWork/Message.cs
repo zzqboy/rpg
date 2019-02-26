de49666eda151d970b50d8a194f05ccd09c09ecc
zzq <ziquanzheng@foxmail.com>
2019-02-26 19:59:59 +0800

    组织目录

---
+++ unity/Assets/Script/NetWork/Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+namespace GameClient
+{
+	// 测试
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct Ping
+	{
+		[MarshalAs(UnmanagedType.I4, SizeConst = 4)]
+		public int id;
+
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)]
+		public char[] name;
+
+		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 20)]
+		public char[] password;
+	}
+
+	class Pack
+	{
+		public static byte[] StructToBytes(object obj)
+		{
+			int size = Marshal.SizeOf(obj);
+			byte[] bytes = new byte[size];
+			IntPtr structPtr = Marshal.AllocHGlobal(size);
+			Marshal.StructureToPtr(obj, structPtr, false);
+			Marshal.Copy(structPtr, bytes, 0, size);
+			Marshal.FreeHGlobal(structPtr);
+
+			return bytes;
+		}
+	}
+}
